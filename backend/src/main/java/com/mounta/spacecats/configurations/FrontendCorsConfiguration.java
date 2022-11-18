bac61c15c5ea2bd8efe9dc107aeb4a9bc87c8047
Coolroo <coolrooswag@gmail.com>
2022-11-17 21:37:50 -0400

    Update FrontendCorsConfiguration.java

--- backend/src/main/java/com/mounta/spacecats/configurations/FrontendCorsConfiguration.java
+++ backend/src/main/java/com/mounta/spacecats/configurations/FrontendCorsConfiguration.java
@@ -1,11 +1,8 @@
 package com.mounta.spacecats.configurations;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class FrontendCorsConfiguration implements WebMvcConfigurer {
